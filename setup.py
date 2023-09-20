0a8b39727c037fefa11ec1410d29bbcd2aa506eb
Andreas Adlichhammer <xyzapip@gmail.com>
2023-09-20 11:11:03 +0200

    Wheel for streamlit installation

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='dashboard_funnel',
-    version='0.1',
-    packages=find_packages()
+    name="dashboard_funnel",
+    version="1.0",
+    packages=["models"],
+    install_requires=[
+        "pydantic>=2.3",
+        "pymc==5.8.1",  # I'm assuming you're using pymc3, if not, specify the correct pymc version.
+        "typing-extensions; python_version>='3.11'",  # This is only needed if you're supporting Python versions <3.8
+    ],
 )
