4cf68061d2cfd72accc2f8d324029795132e6be8
Andreas Adlichhammer <xyzapip@gmail.com>
2023-10-05 12:09:04 +0200

    effect valuation for a&b test added

--- models/a_b_model.py
+++ models/a_b_model.py
@@ -3,6 +3,16 @@ import pymc
 from typing import List
 from .types import BetaPrior
 
+import numpy
+from numpy.typing import NDArray
+
+from pydantic import BaseModel, confloat
+
+class Effect(BaseModel):
+    lift: confloat()
+    confidence: confloat(ge=0, le=1)
+
+
 def create(priors: List[BetaPrior]) -> pymc.Model:
 
     model = pymc.Model()
@@ -20,3 +30,15 @@ def create(priors: List[BetaPrior]) -> pymc.Model:
         p = pymc.Beta('p', alpha=alphas, beta=betas, shape=number_of_variants)
 
     return model
+
+def evaluate_variants(control: NDArray[numpy.float64], variants: NDArray[numpy.float64] ) -> List[Effect]:
+
+    effects = []
+    for variant in variants:
+        lift = variant.mean() - control.mean()
+        confidence = numpy.array([variant - control > 0]).mean()
+        effects.append(Effect(**{'lift': lift, 'confidence': confidence}))
+
+    return effects
+
+
--- tests/models/test_a_b_model.py
+++ tests/models/test_a_b_model.py
@@ -1,9 +1,11 @@
 import pytest
 
-from models.a_b_model import create
+from models.a_b_model import create, evaluate_variants, Effect
 from models.types import BetaPrior
 
 import pymc
+import numpy
+import math
 
 
 @pytest.mark.parametrize('priors', [
@@ -12,6 +14,7 @@ import pymc
      [BetaPrior(**{'alpha': 0, 'beta': 0})]*23,
      [BetaPrior(**{'alpha': 0, 'beta': 0})]*100
 ])
+
 def test_create(priors):
 
     model = create(priors)
@@ -20,3 +23,29 @@ def test_create(priors):
 
     # assert that the beta distribution was created with a shape of n
     assert [rv.type.shape[0] for rv in model.free_RVs if rv.owner.op.name == 'beta'][0] == len(priors)
+
+
+@pytest.mark.parametrize('control, variants, effects', [
+     [
+         [0]*10,
+         [[0]*5+[1]*5],
+         [{'lift':0.5, 'confidence':0.5}]
+     ],
+    [
+        [0.1] * 7 + [0.2] * 3,
+        [[0.15] * 10],
+        [{'lift': 0.02, 'confidence': 0.7}]
+    ],
+
+])
+def test_evaluate_variants(control, variants, effects):
+
+    control = numpy.array(control)
+    variants = numpy.array(variants)
+
+    results = evaluate_variants(control, variants)
+
+
+    for i, e in enumerate(effects):
+         assert(math.isclose(results[i].lift, e['lift']))
+         assert(math.isclose(results[i].confidence, e['confidence']))
